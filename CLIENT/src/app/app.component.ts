4892f426769fada87b445fc14712ef31de663c86
projecteinf <projecteinf@boscdelacoma.cat>
2024-04-17 04:59:11 -0600

    Importar component Jugador

--- CLIENT/src/app/app.component.ts
+++ CLIENT/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { JugadorsComponent} from './projecte/components/jugadors/jugadors.component'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JugadorsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
