3dc1750297098cf01eb7eb8e7ba4c62664dbd70b
projecteinf <projecteinf@boscdelacoma.cat>
2024-04-29 06:14:29 -0600

    Generació taula Error

--- APIERROR/src/schema.ts
+++ APIERROR/src/schema.ts
@@ -1,11 +1,29 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
-import type { GraphQLContext } from './context'
-import { GraphQLError } from 'graphql'
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 
-
+const typeDefinitions =  `
+  type ErrorApp {
+    uid: ID!
+    code: Int!
+    date: DateTime!
+    category: String!
+    message: String!
+    name: String!
+    url: String!
+    user: String!
+  }
+  
+  type Query {
+    ErrorApp(filterNeedle: String, skip: Int, take: Int): [ErrorApp!]!
+  }
+   
+  type Mutation {
+    create(code: ID!, category: String!, url: String!, user: String): ErrorApp!
+  }
+`
  
 export const schema = makeExecutableSchema({
   resolvers: [],
-  typeDefs: []
+  typeDefs: [typeDefinitions]
 })
+
+export default schema
